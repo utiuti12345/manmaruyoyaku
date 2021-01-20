293af4e827dbcf182b8da605c3d10402114f78f6
Yuta Saotome <private.uta.sao@gmail.com>
2021-01-21 00:26:15 +0900

    ingress.yamlと証明書作成関連のファイルを作成

--- build_push.sh
+++ build_push.sh
@@ -1,4 +1,4 @@
 #bin/bash
 
 version=0.1.0
-docker build -t utiuti12345/manamruyoyaku:${version} . && docker push utiuti12345/manamruyoyaku:${version}
+docker build -t utiuti12345/manmaruyoyaku:${version} . && docker push utiuti12345/manmaruyoyaku:${version}
