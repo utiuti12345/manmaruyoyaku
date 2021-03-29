f3c6c457d086850aacd25fb8cf04ead01497a428
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-03-30 00:49:44 +0900

    曜日取得の処理を修正

--- src/main/kotlin/com/manamaru/manmaruyoyaku/domain/FacilitySchedule.kt
+++ src/main/kotlin/com/manamaru/manmaruyoyaku/domain/FacilitySchedule.kt
@@ -1,11 +1,17 @@
 package com.manamaru.manmaruyoyaku.domain
 
 import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
 import java.util.*
+
 
 class FacilitySchedule{
     val facilityId: Int
-    val scheduleDate: Date?
+    val scheduleDate: Date
     val scheduleStartTime: String
     val scheduleEndTime: String
     val scheduleIsAvailable:Boolean
@@ -19,18 +25,19 @@ class FacilitySchedule{
         this.facilityId = facilityId
         // 9時間ズレる問題が環境で解決できなかったので、9時間ズラす
         calendar.time = scheduleDate
-        calendar.add(Calendar.HOUR_OF_DAY,9)
+        //calendar.add(Calendar.HOUR_OF_DAY, 9)
         this.scheduleDate = calendar.getTime()
         this.scheduleStartTime = scheduleStartTime
         this.scheduleEndTime = scheduleEndTime
         this.scheduleIsAvailable = scheduleIsAvailable
     }
 
-    fun formatDatetimeText(isHoliday:Boolean): String {
+    fun formatDatetimeText(isHoliday: Boolean): String {
         val df = SimpleDateFormat("yyyy/MM/dd")
         val formatDate = df.format(calendar.getTime())
         val dayOfWeek = getDayOfWeek()
         val holidayEmoji = if(isHoliday) emoji else ""
+        print("${formatDate}(${dayOfWeek})${scheduleStartTime}〜${scheduleEndTime}${holidayEmoji}")
         return "${formatDate}(${dayOfWeek})${scheduleStartTime}〜${scheduleEndTime}${holidayEmoji}"
     }
 
@@ -38,17 +45,21 @@ class FacilitySchedule{
 
     fun formatDatetimeSort(): Date{
         calendar.time = scheduleDate
-        calendar.add(Calendar.HOUR_OF_DAY,scheduleStartTime.replace("時","").let { it.toInt() })
+        calendar.add(Calendar.HOUR_OF_DAY, scheduleStartTime.replace("時", "").let { it.toInt() })
         return calendar.getTime()
     }
 
     fun getDayOfWeek(): String{
         // 9時間ズレる問題が環境で解決できなかったので、9時間逆にズラす
         calendar.time = scheduleDate
-        calendar.add(Calendar.HOUR_OF_DAY,-9)
-        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1
-        println("${calendar.time} ${dayOfWeek}")
-        return weekName[dayOfWeek]
+        //calendar.add(Calendar.HOUR_OF_DAY, -9)
+
+        val localDate: LocalDate = calendar.time.toInstant().atZone(ZoneId.of("Asia/Tokyo")).toLocalDate()
+        val week: DayOfWeek = localDate.getDayOfWeek()
+        println("${calendar.time}")
+//        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1
+//        println("${calendar.time} ${dayOfWeek}")
+        return week.getDisplayName(TextStyle.SHORT, Locale.JAPANESE)
     }
 
     fun getMonthToString(): String{
